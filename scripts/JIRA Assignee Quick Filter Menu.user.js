bb736ced4a1327a6c2d6f928eccc0b44dc1a8d0c
Jake Savin <jsavin@amazon.com>
2023-08-06 00:39:07 -0700

    Bug fix: Menu is available when switching between plan and work views
    Added a view observer which detects when the view has changed, and builds the menu in the appropriate place in the DOM. There are two sets of Quick Filters(!), one for the work view and one for the plan view, and we now ensure that the menu is available in both views or after switching between them.
    
    Also added code to blur focus from the menu after choosing a filter so that JIRA's hotkeys continue to work instead of being sent to the menu.
    
    Added non-working code to update the menu state after changing views to show the currently active assignee filter. Will fix this later. (See TODO comments.)

--- scripts/JIRA Assignee Quick Filter Menu.user.js
+++ scripts/JIRA Assignee Quick Filter Menu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JIRA Assignee Quick Filter Menu
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Convert assignee Quick Filters into dropdown menu with an "Any" option to clear assignee filter
 // @author       Jake Savin
 // @copyright    Copyright (c) 2023 Jake Savin
@@ -15,27 +15,77 @@
 
 (function() {
     'use strict';
+
     const waitUntilJiraIsLoaded = setInterval(() => {
-        if(document.querySelector('.ghx-controls-filters')) {
+        if (document.getElementById('js-work-quickfilters')) {
             clearInterval(waitUntilJiraIsLoaded);
-            createQuickFiltersDropdown();
+            createQuickFiltersDropdown('board');
+        } else if (document.getElementById('js-plan-quickfilters')) {
+            clearInterval(waitUntilJiraIsLoaded);
+            createQuickFiltersDropdown('backlog');
         }
     }, 500);
+
+    function observeViewChanges() {
+        const ghxPlan = document.getElementById('ghx-plan');
+        if (ghxPlan) {
+            const observer = new MutationObserver(() => {
+                if (ghxPlan.children.length > 0) {
+                    console.log("Switching to backlog view");
+                    createQuickFiltersDropdown('backlog');
+                    updateSelectedFilter('backlog') // TODO not working
+                } else {
+                    console.log("Switching to board view");
+                    createQuickFiltersDropdown('board');
+                    updateSelectedFilter('board') // TODO not working
+                }
+            });
+            observer.observe(ghxPlan, { childList: true });
+        }
+    }
+
+    // Select the correct menu option after switching between backlog and board views
+    function updateSelectedFilter(view) {
+        const filtersDivId = view === 'board' ? 'js-work-quickfilters' : 'js-plan-quickfilters';
+        const filtersDiv = document.getElementById(filtersDivId);
+        const activeFilter = filtersDiv.querySelector('.js-quickfilter-button.ghx-active');
+        const select = document.getElementById('assigneeDropdown');
+
+        if (activeFilter) {
+            select.value = activeFilter.dataset.filterId;
+        } else {
+            select.value = ''; // Default to "Any"
+        }
+    }
+
+    // Initial creation of the dropdown menu based on the current view
+    const currentView = document.getElementById('ghx-plan').children.length > 0 ? 'backlog' : 'board';
+    createQuickFiltersDropdown(currentView);
+
+    // Set up the observer to detect view changes
+    observeViewChanges();
+
 
     // Make a dropdown menu with options for all the filters that appear to be for assignees,
     // including an option at the top called "Any" to clear the assignee filter. Only one
     // assignee filter can be active at a time.
-    function createQuickFiltersDropdown() {
-        const filtersDiv = document.querySelector('.ghx-controls-filters');
-        const filters = Array.from(filtersDiv.querySelectorAll('.js-quickfilter-button'));
+    function createQuickFiltersDropdown(view) {
+        const containerId = view === 'board' ? 'js-work-quickfilters' : 'js-plan-quickfilters';
+        const container = document.getElementById(containerId);
+        if (!container) return; // Exit if the container is not found
+
+        const filters = Array.from(container.querySelectorAll('.js-quickfilter-button'));
 
         // Create dropdown and add 'Clear' option
         const select = document.createElement('select');
-        select.addEventListener('change', () => clearAndSetFilter(select));
+        select.addEventListener('change', () => {
+            clearAndSetFilter(select, view);
+            select.blur(); // Remove focus from the menu
+        });
         const clearOption = document.createElement('option');
         clearOption.text = 'Any';
         select.appendChild(clearOption);
-		select.style.marginRight = '0.6em';
+        select.style.marginRight = '0.6em';
 
         // Create label
         const label = document.createElement('label');
@@ -69,8 +119,9 @@
     }
 
     // Activate the selected filter and deactivate any other assignee filter that was previously active
-    function clearAndSetFilter(select) {
-        const filtersDiv = document.querySelector('.ghx-controls-filters');
+    function clearAndSetFilter(select, view) {
+        const filtersDivId = view === 'board' ? 'js-work-quickfilters' : 'js-plan-quickfilters';
+        const filtersDiv = document.getElementById(filtersDivId);
         const activeFilters = Array.from(filtersDiv.querySelectorAll('.js-quickfilter-button.ghx-active'));
 
         // Clear active filters
